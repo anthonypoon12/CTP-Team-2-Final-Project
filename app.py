02dc4ae38c05be4f300905f8ffbd77635aa5b9bb
anthonypoon12 <anthony@poons.us>
2023-10-08 00:14:00 -0400

    Adding basic imports to a base python file
    Co-authored-by: Jonathan Shields <RedSummit1@users.noreply.github.com>
    Co-authored-by: evanperez444 <evanperez444@users.noreply.github.com>

---
+++ app.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+
+
+
+
+
