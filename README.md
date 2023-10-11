3c3cea18d323f0acd652197d070f92c6d3a9067f
anthonypoon12 <anthony@poons.us>
2023-10-10 22:00:45 -0400

    Basic changes (include downloading csv)

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CTP-Team-2-Final-Project
+
+Before you start:
+
+0. Run 'pip install -r requirements.txt' in your virtual environment
+1. Make a Kaggle account
+2. Get API Token (must be in "kaggle.json")
+3. Place in a directory ".kaggle" in your home directory
+4. Run 'kaggle datasets download -d chitwanmanchanda/fraudulent-transactions-data' in terminal
+5. Run app.py
--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from imblearn.over_sampling import SMOTE
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
+# import numpy as np
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# from sklearn.preprocessing import LabelEncoder
+# from sklearn.model_selection import train_test_split
+# # from imblearn.over_sampling import SMOTE
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.linear_model import LogisticRegression
+from zipfile import ZipFile
 
+file_name = 'fraudulent-transactions-data.zip' #the file is your dataset exact name
+with ZipFile(file_name, 'r') as zip:
+    zip.extractall()
+    print('Done')
 
+df = pd.read_csv('Fraud.csv')
 
-
-
+print(df.head())
